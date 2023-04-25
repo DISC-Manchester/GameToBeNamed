b818effc78b96a41271150ace27b57aeb9e3c255
Phoenixfirewingz <lukethemodded@gmail.com>
2023-04-25 15:05:52 +0100

    Windows - started work on accounts and login

---
+++ src/GameSqlAccess.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace GameToBeNamed.src
+{
+    internal sealed class GameSqlAccess : IDisposable
+    {
+        readonly SqlConnection connection = new SqlConnection("Data Source=10.230.233.180;Initial Catalog=Game_Accounts;User ID=gamequeryer;Password=Password123$");
+        readonly string login_query = "SELECT UserUUID FROM users WHERE UserName=@username AND UserPassword=@password";
+
+        public GameSqlAccess()
+        {
+            try
+            {
+                connection.Open();
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
+            catch(SqlException e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
+        }
+
+        public bool login(string username, string password)
+        {
+            using (SqlCommand command = new SqlCommand(login_query, connection))
+            {
+                // Set the parameters for the query
+                command.Parameters.AddWithValue("@username", username);
+                command.Parameters.AddWithValue("@password", password);
+                return command.ExecuteScalar() != null;
+            }
+        }
+
+        public void Dispose() => connection.Close();
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using GameToBeNamed.src;
+using System;
+
+namespace GameToBeNamed
+{
+    static internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            GameSqlAccess access = new GameSqlAccess();
+            // Method intentionally left empty.
+            Console.WriteLine("Enter your username: ");
+            string username = Console.ReadLine();
+            Console.WriteLine("Enter your password: ");
+            string password = Console.ReadLine();
+            if(access.login(username, password))
+            {
+                Console.WriteLine("logged in");
+            }
+            else 
+            {
+                Console.WriteLine("login failed");
+            }
+        }
+    }
+}
